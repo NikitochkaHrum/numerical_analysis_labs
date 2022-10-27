ed80aa0b01bd1eefe21c6f13675c8f38ccfc03e7
Kirill <91602865+punkosaur@users.noreply.github.com>
2022-10-27 19:09:06 +0500

    Update input_data.h
    I play pokemon go every day

--- src/non_lynear_systems/input_data.h
+++ src/non_lynear_systems/input_data.h
@@ -38,3 +38,26 @@ ld der_F_x_v9(ld x, ld y);
 ld der_F_y_v9(ld x, ld y);
 ld der_F_x_v25(ld x, ld y);
 ld der_F_y_v25(ld x, ld y);
+
+
+//проверка 20 варианта
+ld f1_v20(ld x, ld y);
+ld f2_v20(ld x, ld y);
+ld fi1_v20(ld x, ld y);
+ld fi2_v20(ld x, ld y);
+
+ld F_v20(ld x, ld y);
+
+// Частные производные
+ld der_fi1_x_v20(ld x, ld y);
+ld der_fi1_y_v20(ld x, ld y);
+ld der_fi2_x_v20(ld x, ld y);
+ld der_fi2_y_v20(ld x, ld y);
+
+ld der_f1_x_v20(ld x, ld y);
+ld der_f1_y_v20(ld x, ld y);
+ld der_f2_x_v20(ld x, ld y);
+ld der_f2_y_v20(ld x, ld y);
+
+ld der_F_x_v20(ld x, ld y);
+ld der_F_y_v20(ld x, ld y);
