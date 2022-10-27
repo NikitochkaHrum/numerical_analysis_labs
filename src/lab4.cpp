e42060d4aafd81c3aab6a3a094979e7988e8d8d9
ponkinikita <ponkin.nikitka@mail.ru>
2022-10-27 22:51:53 +0500

    fixes

--- src/lab4.cpp
+++ src/lab4.cpp
@@ -9,15 +9,16 @@ int main(){
     const ld EPS1 = 1e-12;
     const ld EPS2 = 1e-4;
     // Vec init({-0.95, -1.1}); // v9
-    Vec init({-0.15, -0.85}); // v25
+    //Vec init({-0.15, -0.85}); // v25
+    Vec init({-1.7, 1.3}); // vovavar
     cout << "Начальное приближение:\nx = " << init[0] << "\ny = " << init[1] << '\n';
 
-    auto x = newton_method(init, f1_v25, f2_v25, der_f1_x_v25, der_f1_y_v25, der_f2_x_v25, der_f2_y_v25, EPS1);
+    auto x = newton_method(init, f1_v20, f2_v20, der_f1_x_v20, der_f1_y_v20, der_f2_x_v20, der_f2_y_v20, EPS1);
     cout << fixed << setprecision(10) << "x = " << x[0] << "\ny = " << x[1] << '\n';
 
-    auto x_it = simple_iteration(init, x, f1_v25, f2_v25, fi1_v25, fi2_v25, der_fi1_x_v25, der_fi1_y_v25, der_fi2_x_v25, der_fi2_y_v25, EPS2);
+    auto x_it = simple_iteration(init, x, f1_v20, f2_v20, fi1_v20, fi2_v20, der_fi1_x_v20, der_fi1_y_v20, der_fi2_x_v20, der_fi2_y_v20, EPS2);
     cout << fixed << setprecision(10) << "x = " << x_it[0] << "\ny = " << x_it[1] << '\n';
 
-    x_it = gradient_descent(init, x, f1_v25, f2_v25, der_f1_x_v25, der_f1_y_v25, der_f2_x_v25, der_f2_y_v25, F_v25, der_F_x_v25, der_F_y_v25, EPS2);
+    x_it = gradient_descent(init, x, f1_v20, f2_v20, der_f1_x_v20, der_f1_y_v20, der_f2_x_v20, der_f2_y_v20, F_v20, der_F_x_v20, der_F_y_v20, EPS2);
     cout << fixed << setprecision(10) << "x = " << x_it[0] << "\ny = " << x_it[1] << '\n';
 }
--- src/non_lynear_systems/input_data.cpp
+++ src/non_lynear_systems/input_data.cpp
@@ -1,121 +1,181 @@
 #include "input_data.h"
 
-ld f1_v9(ld x, ld y){
+ld f1_v9(ld x, ld y) {
     return cos(x + 0.5) - y - 2.;
 }
 
-ld f2_v9(ld x, ld y){
-    return sin(y) - 2*x - 1;
+ld f2_v9(ld x, ld y) {
+    return sin(y) - 2 * x - 1;
 }
 
-ld f1_v25(ld x, ld y){
+ld f1_v25(ld x, ld y) {
     return cos(y + 0.5) + x - 0.8;
 }
 
-ld f2_v25(ld x, ld y){
-    return sin(x) - 2*y - 1.6;
+ld f2_v25(ld x, ld y) {
+    return sin(x) - 2 * y - 1.6;
 }
 
-ld fi1_v9(ld x, ld y){
-    return (sin(y) - 1.)/2.;
+ld f1_v20(ld x, ld y) {
+    return sin(y + 2) - x - 1.5;
 }
 
-ld fi2_v9(ld x, ld y){
+ld f2_v20(ld x, ld y) {
+    return y + cos(x - 2) - 0.5;
+}
+
+ld fi1_v9(ld x, ld y) {
+    return (sin(y) - 1.) / 2.;
+}
+
+ld fi2_v9(ld x, ld y) {
     return cos(x + 0.5) - 2.;
 }
 
-ld fi1_v25(ld x, ld y){
+ld fi1_v25(ld x, ld y) {
     return 0.8 - cos(y + 0.5);
 }
 
-ld fi2_v25(ld x, ld y){
-    return (sin(x) - 1.6)/2.;
+ld fi2_v25(ld x, ld y) {
+    return (sin(x) - 1.6) / 2.;
 }
 
-ld F_v9(ld x, ld y){
-    return pow(f1_v9(x, y), 2) +  pow(f2_v9(x, y), 2);
+ld fi1_v20(ld x, ld y) {
+    return sin(y + 2) - 1.5;
 }
 
-ld F_v25(ld x, ld y){
-    return pow(f1_v25(x, y), 2) +  pow(f2_v25(x, y), 2);
+ld fi2_v20(ld x, ld y) {
+    return 0.5 - cos(x - 2);
 }
 
-ld der_fi1_x_v9(ld x, ld y){
+ld F_v9(ld x, ld y) {
+    return pow(f1_v9(x, y), 2) + pow(f2_v9(x, y), 2);
+}
+
+ld F_v25(ld x, ld y) {
+    return pow(f1_v25(x, y), 2) + pow(f2_v25(x, y), 2);
+}
+
+ld F_v20(ld x, ld y) {
+    return pow(f1_v20(x, y), 2) + pow(f2_v20(x, y), 2);
+}
+
+ld der_fi1_x_v9(ld x, ld y) {
     return 0;
 }
 
-ld der_fi1_y_v9(ld x, ld y){
-    return cos(y)/2.;
+ld der_fi1_y_v9(ld x, ld y) {
+    return cos(y) / 2.;
 }
 
-ld der_fi2_x_v9(ld x, ld y){
+ld der_fi2_x_v9(ld x, ld y) {
     return -sin(x + 0.5);
 }
 
-ld der_fi2_y_v9(ld x, ld y){
+ld der_fi2_y_v9(ld x, ld y) {
     return 0;
 }
 
-ld der_fi1_x_v25(ld x, ld y){
+ld der_fi1_x_v25(ld x, ld y) {
     return 0;
 }
 
-ld der_fi1_y_v25(ld x, ld y){
+ld der_fi1_y_v25(ld x, ld y) {
     return sin(y + 0.5);
 }
 
-ld der_fi2_x_v25(ld x, ld y){
-    return cos(x)/2.;
+ld der_fi2_x_v25(ld x, ld y) {
+    return cos(x) / 2.;
 }
 
-ld der_fi2_y_v25(ld x, ld y){
+ld der_fi2_y_v25(ld x, ld y) {
     return 0;
 }
 
-ld der_f1_x_v9(ld x, ld y){
+ld der_fi1_x_v20(ld x, ld y) {
+    return 0;
+}
+
+ld der_fi1_y_v20(ld x, ld y) {
+    return cos(y + 2);
+}
+
+ld der_fi2_x_v20(ld x, ld y) {
+    return sin(x - 2);
+}
+
+ld der_fi2_y_v20(ld x, ld y) {
+    return 0;
+}
+
+ld der_f1_x_v9(ld x, ld y) {
     return -sin(x + 0.5);
 }
 
-ld der_f1_y_v9(ld x, ld y){
+ld der_f1_y_v9(ld x, ld y) {
     return -1;
 }
 
-ld der_f2_x_v9(ld x, ld y){
+ld der_f2_x_v9(ld x, ld y) {
     return -2;
 }
 
-ld der_f2_y_v9(ld x, ld y){
+ld der_f2_y_v9(ld x, ld y) {
     return cos(y);
 }
 
-ld der_f1_x_v25(ld x, ld y){
+ld der_f1_x_v25(ld x, ld y) {
     return 1;
 }
 
-ld der_f1_y_v25(ld x, ld y){
+ld der_f1_y_v25(ld x, ld y) {
     return -sin(y + 0.5);
 }
 
-ld der_f2_x_v25(ld x, ld y){
+ld der_f2_x_v25(ld x, ld y) {
     return cos(x);
 }
 
-ld der_f2_y_v25(ld x, ld y){
+ld der_f2_y_v25(ld x, ld y) {
     return -2;
 }
 
-ld der_F_x_v9(ld x, ld y){
-    return 2*f1_v9(x, y)*der_f1_x_v9(x, y) + 2*f2_v9(x, y)*der_f2_x_v9(x, y);
+ld der_f1_x_v20(ld x, ld y) {
+    return -1;
 }
 
-ld der_F_y_v9(ld x, ld y){
-    return 2*f1_v9(x, y)*der_f1_y_v9(x, y) + 2*f2_v9(x, y)*der_f2_y_v9(x, y);
+ld der_f1_y_v20(ld x, ld y) {
+    return cos(y + 2);
 }
 
-ld der_F_x_v25(ld x, ld y){
-    return 2*f1_v25(x, y)*der_f1_x_v25(x, y) + 2*f2_v25(x, y)*der_f2_x_v25(x, y);
+ld der_f2_x_v20(ld x, ld y) {
+    return -sin(x - 2);
 }
 
-ld der_F_y_v25(ld x, ld y){
-    return 2*f1_v25(x, y)*der_f1_y_v25(x, y) + 2*f2_v25(x, y)*der_f2_y_v25(x, y);
+ld der_f2_y_v20(ld x, ld y) {
+    return 1;
+}
+
+ld der_F_x_v9(ld x, ld y) {
+    return 2 * f1_v9(x, y) * der_f1_x_v9(x, y) + 2 * f2_v9(x, y) * der_f2_x_v9(x, y);
+}
+
+ld der_F_y_v9(ld x, ld y) {
+    return 2 * f1_v9(x, y) * der_f1_y_v9(x, y) + 2 * f2_v9(x, y) * der_f2_y_v9(x, y);
+}
+
+ld der_F_x_v25(ld x, ld y) {
+    return 2 * f1_v25(x, y) * der_f1_x_v25(x, y) + 2 * f2_v25(x, y) * der_f2_x_v25(x, y);
+}
+
+ld der_F_y_v25(ld x, ld y) {
+    return 2 * f1_v25(x, y) * der_f1_y_v25(x, y) + 2 * f2_v25(x, y) * der_f2_y_v25(x, y);
+}
+
+ld der_F_x_v20(ld x, ld y) {
+    return 2 * f1_v20(x, y) * der_f1_x_v20(x, y) + 2 * f2_v20(x, y) * der_f2_x_v20(x, y);
+}
+
+ld der_F_y_v20(ld x, ld y) {
+    return 2 * f1_v20(x, y) * der_f1_y_v20(x, y) + 2 * f2_v20(x, y) * der_f2_y_v20(x, y);
 }
